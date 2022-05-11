1024f1ef7878cf86fb5a0c9a0cd954584a7194c2
guest01 <guest01@ITELABD28>
2022-05-10 22:20:26 -0300

    10.05.22

---
+++ Exercícios/exercício12/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace exercício12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] nome = new string[10];
+
+            for (int i = 0; i < nome.Length; i++)
+            {
+
+                Console.WriteLine("insira um nome: ");
+                nome[i] = Convert.ToString(Console.ReadLine());
+
+            }
+            Console.WriteLine(nome[0] + " ou " + nome[1] + " ou " + nome[2] + " ou " + nome[3] + " ou " + nome[4] + " ou " + nome[5] + " ou " + nome[6] + " ou "
+                + nome[7] + " ou " + nome[8] + " ou " + nome[9]);
+
+        }
+    }
+}
---
+++ Exercícios/exercício13/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace exercício13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vetor1 = new int[5];
+            int[] vetor2 = new int[5];
+            int[] vetor3 = new int[5];
+
+            for (int i = 0; i < vetor1.Length; i++)
+            {
+                Console.WriteLine("Primeiro vetor: ");
+                vetor1[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < vetor2.Length; i++)
+            {
+                Console.WriteLine("Segundo vetor: ");
+                vetor2[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            for (int i = 0; i < vetor3.Length; i++)
+            {
+                vetor3[i] = vetor1[i] * vetor2[i];
+                Console.WriteLine("Multiplicação entre os vetores: " + vetor3[i]);
+            }
+        }
+    }
+}
