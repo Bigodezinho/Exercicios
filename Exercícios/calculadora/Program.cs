fccaee7d42f031444553bbe59f201d8365e8691c
guest01 <guest01@ITELABD28>
2022-05-12 22:13:09 -0300

    12.05.22

---
+++ Exercícios/calculadora/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("1- Adição, 2- Subtração, 3- Multiplicação, 4- Divisão, 0- Sair");
+            int escolhaUsuario = Convert.ToInt32(Console.ReadLine()); 
+            Console.WriteLine("Insira o primeiro numero:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira o segundo numero:");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+        
+        static int SomarNumeros(int a, int b)
+        {
+            return a + b;
+        }
+        static int subtrairNumeros(int a, int b)
+        {
+            return a - b;
+        }
+        static int multiplicarNumeros(int a, int b)
+        {
+            return a * b;
+        }
+        static int dividirNumeros(int a, int b)
+        {
+            return a / b;
+        }
+    }
+}
