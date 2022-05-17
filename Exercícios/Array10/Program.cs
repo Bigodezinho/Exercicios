111ba5f2d5714ae01b18747230f9a041c4e68e8a
guest01 <guest01@ITELABD28>
2022-05-16 22:15:15 -0300

    16.05.22

---
+++ Exercícios/Array10/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Array10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+
+
+
+            //double[] array10 = new double[10];
+
+            //contador(array10);
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    for (int j = 0; j < 10; j++)
+            //    {
+            //        array10[j] = Convert.ToDouble(Console.ReadLine());
+
+            //        if (array10[i] > array10[j])
+            //        {
+            //            Console.WriteLine(array10[i]);
+            //        }
+            //    }
+            //}
+
+            //static void contador(double[] a)
+            //{
+            //    for (int i = 0; i < 10; i++)
+            //    {
+            //        Console.WriteLine("Insira 10 números:");
+            //        a[i] = Convert.ToDouble(Console.ReadLine());
+            //    }
+            //}
+        }
+    }
+}
--- Exercícios/calculadora/Program.cs
+++ Exercícios/calculadora/Program.cs
@@ -4,32 +4,74 @@ namespace calculadora
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("1- Adição, 2- Subtração, 3- Multiplicação, 4- Divisão, 0- Sair");
-            int escolhaUsuario = Convert.ToInt32(Console.ReadLine()); 
-            Console.WriteLine("Insira o primeiro numero:");
-            int x = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Insira o segundo numero:");
-            int y = Convert.ToInt32(Console.ReadLine());
 
-        
-        static int SomarNumeros(int a, int b)
+            Console.WriteLine("1- Adição, 2- Subtração, 3- Multiplicação, 4- Divisão, 0- Sair");
+            double escolhaUsuario = Convert.ToDouble(Console.ReadLine());
+            while (escolhaUsuario != 0 && escolhaUsuario <= 4 && escolhaUsuario >= 1)
+            {
+
+                if (escolhaUsuario == 1) 
+                {
+                    Console.WriteLine("Insira o primeiro numero:");
+                    double x = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Insira o segundo numero:");
+                    double y = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("O resultado da soma é " + adicao(x, y));
+                }
+                if (escolhaUsuario == 2)
+                {
+                    Console.WriteLine("Insira o primeiro numero:");
+                    double x = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Insira o segundo numero:");
+                    double y = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("O resultado da soma é " + subtracao(x, y));
+                }
+                if (escolhaUsuario == 3)
+                {
+                    Console.WriteLine("Insira o primeiro numero:");
+                    double x = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Insira o segundo numero:");
+                    double y = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("O resultado da soma é " + multiplicacao(x, y));
+                }
+                if (escolhaUsuario == 4)
+                {
+                    Console.WriteLine("Insira o primeiro numero:");
+                    double x = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("Insira o segundo numero:");
+                    double y = Convert.ToDouble(Console.ReadLine());
+                    Console.WriteLine("O resultado da soma é " + divisao(x, y));
+                }
+
+                Console.WriteLine("1- Adição, 2- Subtração, 3- Multiplicação, 4- Divisão, 0- Sair");
+                escolhaUsuario = Convert.ToDouble(Console.ReadLine());
+            }
+            if (escolhaUsuario > 4 || escolhaUsuario < 1)
+            {
+                Console.WriteLine("Operação não encontrada.");
+            }
+
+        }
+
+        static double adicao(double a, double b)
         {
             return a + b;
         }
-        static int subtrairNumeros(int a, int b)
+        static double subtracao(double a, double b)
         {
             return a - b;
         }
-        static int multiplicarNumeros(int a, int b)
+        static double multiplicacao(double a, double b)
         {
             return a * b;
         }
-        static int dividirNumeros(int a, int b)
+        static double divisao(double a, double b)
         {
             return a / b;
-        }
+        }        
     }
 }
+
